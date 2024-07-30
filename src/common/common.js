1b3a3b8e962ea56efdfed8a74cb8e7f23fcfd521
Elizaveta Guseva <liza.guseva@hey.com>
2024-07-30 15:48:29 +0200

    some forgotten files

---
+++ src/common/common.js
@@ -0,0 +1,9 @@
+
+// Locating files in Garmin data dump
+
+// Headers
+export const WELLNESS_HEADER = 'DI_CONNECT/DI-Connect-Wellness'
+export const ENDURANCE_HEADER = 'DI_CONNECT/DI-Connect-Metrics/EnduranceScore'
+
+// Tails
+export const SLEEP_TAIL = "sleepData.json"
---
+++ src/components/UnzipNPlot.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import JSZip from "jszip";
+import { StatusEnum } from "./Common";
+import TheGraphs from "./TheGraphs";
+import { 
+    WELLNESS_HEADER, 
+    ENDURANCE_HEADER, 
+    SLEEP_TAIL
+} from "../common/common";
+
+
+function getEnudranceScoreFiles(filesObject) {
+    return filesObject.filter((file) => file.name.startsWith(ENDURANCE_HEADER));
+}
+
+function getSleepDataFiles(filesObject) {
+    return filesObject.filter((file) => file.name.startsWith(WELLNESS_HEADER) && file.name.endsWith(SLEEP_TAIL));
+}
+
+
+const LoadTheFile = ({status, error, handleUpload, isLoaded}) => {
+    return (<div className={`p-4 transition-transform duration-500 ${isLoaded ? 'scale-0' : 'scale-100'}`}>
+        <h1 className="font-bold">Upload the zip file data dump Garmin sent you</h1>
+        <input className="mt-4" type="file" onChange={handleUpload} />
+        {error && <div className="text-red-500 mt-2">Error: {error.message}</div>}
+        {status === StatusEnum.LOADING && <div className="mt-4 ml-4 font-bold">Loading...</div>}
+    </div>)
+}
+
+
+const LoadAnotherFile = ({ isLoaded, toggleIsLoaded }) => {
+    return (
+        <div className={`flex justify-end h-16 mt-4 transition-transform duration-500 ${isLoaded ? 'scale-100' : 'scale-0'}`}>
+            <button className="btn btnboring" onClick={toggleIsLoaded}>
+                {'Load Another File'}
+            </button>
+        </div>
+    );
+};
+
+
+const LoadingComponent = ({status, error, handleUpload, selectFiles}) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (status === StatusEnum.LOADED) {
+            setTimeout(() => {
+                setIsLoaded(true);
+            }, 300); // wait for the transition to complete
+        }
+    }, [status]);
+
+    const toggleIsLoaded = () => {
+        setIsLoaded((prev) => !prev);
+    };
+
+    return (
+        <div>
+            {/* if is loaded show LoadTheFile if is not loaded show LoadAnotheFile */}
+            {isLoaded ? (
+                <div>
+                <LoadAnotherFile isLoaded={isLoaded} toggleIsLoaded={toggleIsLoaded} />
+                <TheGraphs selectFiles={selectFiles} />
+                </div>
+            ) : (
+                <LoadTheFile 
+                status={status} 
+                error={error} 
+                handleUpload={handleUpload} 
+                isLoaded={isLoaded} />
+            )}
+        
+        </div>
+    );
+};
+
+
+
+LoadingComponent.propTypes = {
+    status: PropTypes.string.isRequired,
+    error: PropTypes.object,
+    handleUpload: PropTypes.func.isRequired,
+    selectFiles: PropTypes.object.isRequired,
+};
+
+LoadAnotherFile.propTypes = {
+    isLoaded: PropTypes.bool.isRequired,
+    toggleIsLoaded: PropTypes.func.isRequired,
+};
+
+LoadTheFile.propTypes = {
+    status: PropTypes.string.isRequired,
+    error: PropTypes.object,
+    handleUpload: PropTypes.func.isRequired,
+    isLoaded: PropTypes.bool.isRequired,
+};
+
+
+
+
+
+
+
+const UnzipNPlot = () => {
+    const [filesObject, setFilesObject] = useState([]);
+    const [status, setStatus] = useState(StatusEnum.NOT_STARTED);
+    const [error, setError] = useState(null);
+    const [selectFiles, setSelectFiles] = useState(null);
+
+    const handleUpload = (e) => {
+        setStatus(StatusEnum.LOADING);
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        const readFileAsArrayBuffer = (file) => {
+            return new Promise((resolve, reject) => {
+                reader.onload = () => resolve(reader.result);
+                reader.onerror = () => reject(reader.error);
+                reader.readAsArrayBuffer(file);
+            });
+        };
+
+        readFileAsArrayBuffer(file)
+            .then(buffer => JSZip.loadAsync(buffer))
+            .then(zipFile => {
+                const files = Object.values(zipFile.files);
+                return Promise.all(
+                    files.map(file =>
+                        file.async("blob").then(fileData => ({
+                            name: file.name,
+                            data: fileData,
+                        }))
+                    )
+                );
+            })
+            .then(filesObject => {
+                setFilesObject(filesObject);
+
+                setSelectFiles({
+                    'sleep': getSleepDataFiles(filesObject),
+                    'endurance': getEnudranceScoreFiles(filesObject)
+                });
+                setStatus(StatusEnum.LOADED);
+            })
+            .catch(err => setError(err));
+    };
+
+    return LoadingComponent({status, error, handleUpload, selectFiles});
+};
+
+export { UnzipNPlot };
